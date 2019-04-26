83e168245fc22d21568f850c5dcad11645627e54
georuiz817 <georuiz817@gmail.com>
2019-04-26 02:23:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,13 +22,4 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   }
   
-  if (string.toUpperCase() === string) {
-    return "YES INDEED!"
-  }
-
-  if (string === "I love you, Grandma.") {
-    return "I love you, too."
-  }
-
-  return "Are you eating enough?"
-}
+ learn test
