50464a012c4d16554ce86cf0d5eaf7a69ed8560f
georuiz817 <georuiz817@gmail.com>
2019-04-26 01:43:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string){
   console.log("I can't hear you!");
-  return console.log.mixedcase.toLowerCase
+  return console.log.mixedCase.toLowerCase
   
 }
